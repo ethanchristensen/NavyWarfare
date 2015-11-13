b842f259014b4543d2c32336e96dde7591438c76
Ethan Christensen <Ethan@Ethans-MBP.hsd1.mi.comcast.net>
2015-11-13 18:25:01 -0500

    Added Parse user login

--- NavyWarfare/AppDelegate.swift
+++ NavyWarfare/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ParseFacebookUtilsV4
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -27,7 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // [Optional] Track statistics around application opens.
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
-
+        
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
         
         return true
     }
@@ -46,12 +48,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
 
---
+++ NavyWarfare/HomeViewController.swift
@@ -0,0 +1,93 @@
+//
+//  LoginViewController.swift
+//  NavyWarfare
+//
+//  Created by Ethan Christensen on 11/12/15.
+//  Copyright © 2015 Ethan Christensen. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class HomeViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
+
+    @IBOutlet weak var userName: UILabel!
+    
+    
+    let loginViewController = PFLogInViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setup()
+        
+    }
+    
+    
+    func logInViewController(logInController: PFLogInViewController, shouldBeginLogInWithUsername username: String, password: String) -> Bool {
+        
+        if (!username.isEmpty || !password.isEmpty) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
+        let alert = UIAlertController(title: "Login Error", message: "Please login to Twitter", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+        logInController.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
+        
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didFailToSignUpWithError error: NSError?) {
+        
+        print("Failed to sign up...")
+    }
+    
+    func setup(){
+        if PFUser.currentUser() == nil {
+            
+            let loginViewController = PFLogInViewController()
+            loginViewController.delegate = self
+            loginViewController.fields = [.UsernameAndPassword, .LogInButton, .SignUpButton, .PasswordForgotten, .Facebook]
+            self.presentViewController(loginViewController, animated: true, completion: nil)
+            
+            let signupViewController = PFSignUpViewController()
+            signupViewController.delegate = self
+            
+        }else{
+            self.userName.text = "Loged In as " + (PFUser.currentUser()?.username)!
+        }
+        
+    }
+    
+    @IBAction func logOutButtonAction(sender: UIButton) {
+        PFUser.logOut()
+        self.setup()
+    }
+
+    
+
+}
