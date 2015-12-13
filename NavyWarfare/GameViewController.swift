f97955b513929696741c2c58cedab5021f330733
Gerad Wegener <wegenerg@mail.gvsu.edu>
2015-12-13 15:06:50 -0500

    The stuff

--- NavyWarfare/GameViewController.swift
+++ NavyWarfare/GameViewController.swift
@@ -13,18 +13,30 @@ import SceneKit
 class GameViewController: UIViewController, UIScrollViewDelegate {
     
     var scene = SCNScene()
+    var worldNode = SCNNode()
     var cameraNode = SCNNode()
     var lastRotation = CGFloat()
     var rotateGesture = UIRotationGestureRecognizer()
+    var lastSelectedShip = String()
+    var attacking = Bool()
+    var attackDamage = Int()
+    var ShipsHealth = ["BattleshipSupporter": 100, "BattleshipCruiser": 100]
+    var ShipsDamage = ["BattleshipSupporter": 40, "BattleshipCruiser": 40]
+    var playerTurn = Bool()
+    
+    @IBOutlet weak var gameView: UIView!
+    
+    @IBOutlet weak var controllerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        lastSelectedShip = ""
         // create a new scene
         scene = SCNScene(named: "art.scnassets/Small Tropical Island.scn")!
         // create and add a camera to the scene
-        cameraNode = scene.rootNode.childNodeWithName("camera", recursively: true)!
         cameraNode.camera = SCNCamera()
-        //cameraNode.camera!.usesOrthographicProjection = true
+        cameraNode.camera!.usesOrthographicProjection = true
+        cameraNode.camera!.orthographicScale = 4;
         cameraNode.camera!.zNear = 0
         cameraNode.camera!.zFar = 100
         scene.rootNode.addChildNode(cameraNode)
@@ -38,7 +50,7 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 30, z: 0)
+        lightNode.position = SCNVector3(x: 0, y: 20, z: 0)
         //scene.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
@@ -65,8 +77,9 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
         
         
         // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
+        let scnView = self.gameView as! SCNView
+        //self.view.sendSubviewToBack(self.controllerView)
+        self.controllerView.hidden = true
         // set the scene to the view
         scnView.scene = scene
         
@@ -84,14 +97,22 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
         let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
         scnView.addGestureRecognizer(tapGesture)
         
-        // add a swipe gesture recognizer
-        rotateGesture = UIRotationGestureRecognizer(target: self, action: "handleRotate:")
-        scnView.addGestureRecognizer(rotateGesture)
+        // add a pan gesture recognizer
+        let panGesture = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        scnView.addGestureRecognizer(panGesture)
         
         
+        let ship1 = scene.rootNode.childNodeWithName("BattleshipSupporter", recursively: true)!
+        
+        let ship2 = scene.rootNode.childNodeWithName("BattleshipCruiser", recursively: true)!
+        worldNode.addChildNode(ship)
+        worldNode.addChildNode(ship1)
+        worldNode.addChildNode(ship2)
+        
+        scene.rootNode.addChildNode(worldNode)
         //scrollView.contentSize = cameraNode.camera!.accessibilityFrame.size
-       // scrollView.addSubview(cameraNode.camera!.)
-
+        // scrollView.addSubview(cameraNode.camera!.)
+        
         //scnView.addSubview(scrollView)
         //TODO:
         //self.view.bringSubviewToFront
@@ -104,13 +125,16 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
-    func rotatedView(sender:UIRotationGestureRecognizer){
-        var lastRotation = CGFloat()
-        
-        if(sender.state == UIGestureRecognizerState.Ended){
-            lastRotation = 0.0;
+    func handlePan(sender:UIPanGestureRecognizer){
+        let translation = sender.translationInView(self.view)
+        if let view = sender.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                y:view.center.y + translation.y)
         }
-        let rotation = 0.0 - (lastRotation - sender.rotation)
+        sender.setTranslation(CGPointZero, inView: self.view)
+       
+        //worldNode.position = CGPoint(x:view.center.x + translation.x,
+         //   y:view.center.y + translation.y)
         //var point = rotateGesture.locationInView()
         //var currentTrans = sender.view.transform
         //var newTrans = CGAffineTransformRotate(currentTrans, rotation)
@@ -128,7 +152,9 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
     
     func handleTap(gestureRecognize: UIGestureRecognizer) {
         // retrieve the SCNView
-        let scnView = self.view as! SCNView
+        let scnView = self.gameView as! SCNView
+        
+        self.controllerView.hidden = false
         
         // check what nodes are tapped
         let p = gestureRecognize.locationInView(scnView)
@@ -137,34 +163,57 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
         if hitResults.count > 0 {
             // retrieved the first clicked object
             let result: AnyObject! = hitResults[0]
-            if(result.name == "Battleship1"){
-                let moveUp = SCNAction.moveByX(0.5, y: 0.0, z: 0.5, duration: 2.0)
-                result.runAction(moveUp)
-            }
-            // get its material
-            /*let material = result.node!.geometry!.firstMaterial!
             
+            //            if(result.name == "Battleship1"){
+            //                let moveUp = SCNAction.moveByX(0.5, y: 0.0, z: 0.5, duration: 2.0)
+            //                result.runAction(moveUp)
+            //            }
+            // get its material
+            let material = result.node!.geometry!.firstMaterial!
+            let selectedNode = result.node!
+            if(selectedNode.name!.containsString("Battleship")){
+                lastSelectedShip = selectedNode.name!
+                lastSelectedShip += "er"
+                if(attacking){
+                    doDamage()
+                }
+            }
+            attacking = false
             // highlight it
             SCNTransaction.begin()
             SCNTransaction.setAnimationDuration(0.5)
             
             // on completion - unhighlight
             SCNTransaction.setCompletionBlock {
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                material.emission.contents = UIColor.blackColor()
-                
-                SCNTransaction.commit()
+            SCNTransaction.begin()
+            SCNTransaction.setAnimationDuration(0.5)
+            
+            material.emission.contents = UIColor.blackColor()
+            
+            SCNTransaction.commit()
             }
             
             material.emission.contents = UIColor.redColor()
             
             SCNTransaction.commit()
-        }*/
-    
         }
+        
+    }
     
+    func doDamage(){
+        let ship = scene.rootNode.childNodeWithName(self.lastSelectedShip, recursively: true)
+        if(ship != nil){
+            ShipsHealth.updateValue(ShipsHealth[lastSelectedShip]! - attackDamage, forKey: lastSelectedShip)
+            if(ShipsHealth[lastSelectedShip]! <= 0){
+                destroyShip(ship!)
+            }
+        }
+    }
+    
+    func destroyShip(ship: SCNNode){
+        let moveUp = SCNAction.moveByX(0.0, y: -1.0, z: 0.0, duration: 2.0)
+        ship.pivot = SCNMatrix4MakeRotation(Float(M_PI_4*2), 1, 0, 0)
+        ship.runAction(moveUp)
     }
     
     func degToRad(deg: Float)->Float {
@@ -192,4 +241,64 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
         // Release any cached data, images, etc that aren't in use.
     }
     
+    //Moves ship negative on the z
+    @IBAction func forwardMovementAction(sender: AnyObject) {
+        let ship = scene.rootNode.childNodeWithName(self.lastSelectedShip, recursively: true)
+        
+        if(ship != nil){
+           // let rotate = SCNAction.rotateToAxisAngle(<#T##axisAngle: SCNVector4##SCNVector4#>, duration: <#T##NSTimeInterval#>)
+            let moveUp = SCNAction.moveByX(0.0, y: 0.0, z: -0.5, duration: 1.0)
+            //ship!.runAction(rotate)
+        ship?.pivot = SCNMatrix4MakeRotation(Float(M_PI_2*2), 0, 1, 0)
+            ship!.runAction(moveUp)
+        }
+    }
+    
+    @IBAction func leftMovementAction(sender: AnyObject) {
+        let ship = scene.rootNode.childNodeWithName(self.lastSelectedShip, recursively: true)
+        if(ship != nil){
+            let moveUp = SCNAction.moveByX(-0.5, y: 0.0, z: -0.0, duration: 1.0)
+            ship?.pivot = SCNMatrix4MakeRotation(Float(-M_PI_2), 0, 1, 0)
+            ship!.runAction(moveUp)
+        }
+    }
+    
+    @IBAction func backMovementAction(sender: AnyObject) {
+        let ship = scene.rootNode.childNodeWithName(self.lastSelectedShip, recursively: true)
+        if(ship != nil){
+            let moveUp = SCNAction.moveByX(0.0, y: 0.0, z: 0.5, duration: 1.0)
+            ship?.pivot = SCNMatrix4MakeRotation(Float(M_PI_2*4), 0, 1, 0)
+            ship!.runAction(moveUp)
+        }
+    }
+    
+    @IBAction func rightMovementAction(sender: AnyObject) {
+        let ship = scene.rootNode.childNodeWithName(self.lastSelectedShip, recursively: true)
+        if(ship != nil){
+            let moveUp = SCNAction.moveByX(0.5, y: 0.0, z: 0.0, duration: 1.0)
+            ship?.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 0, 1, 0)
+            ship!.runAction(moveUp)
+            
+        }
+    }
+    
+    
+    @IBAction func attackButtonAction(sender: AnyObject) {
+        attacking = true
+        attackDamage = ShipsDamage[lastSelectedShip]!
+    }
+    
+    @IBAction func cancelButtonAction(sender: AnyObject) {
+        self.controllerView.hidden = true
+        attacking = false
+        lastSelectedShip = ""
+    }
+    
+    func createGameObject(){
+        var game = PFObject(className: "Game")
+        game["player1"] = PFUser.currentUser()!.objectId
+        game["turn"] = true
+        game["ship"] = ""
+        //game[]
+    }
 }
